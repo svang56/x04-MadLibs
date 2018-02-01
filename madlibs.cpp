1e1b8a963844b029ebff981fdebd161d4d65d7d7
svang56 <svang56@mail.csuchico.edu>
2018-01-31 17:58:46 -0800

    MadLibs Lab Program

--- madlibs.cpp
+++ madlibs.cpp
@@ -10,25 +10,25 @@ int main()
 
     std::string activity;
     std::cout<< "What is an activity you like to do?" << std::endl;
-    std::cin>> actvity;
+    std::cin>> activity;
 
     std::string number;
-    std::cout<< "Give me a number between 5-50" << std;endl;
+    std::cout<< "Give me a number between 5-50." << std::endl;
     std::cin>> number;
 
     std::string symbol;
-    std::cout<< "Give me an exclamation mark or question mark" << std::endl;
+    std::cout<< "Give me an question mark or exclamation mark." << std::endl;
     std::cin>> symbol;
 
     std::string decimal;
-    std::cout<< "Give me a decimal number" << std::endl;
+    std::cout<< "Give me a decimal number between 5 and 20." << std::endl;
     std::cin>> decimal;
     
-    std::cout<< "Hi, my name is" << name << std::endl;
-    std::cout<< "My favorite thing to do is" << activity << std::endl;
-    std::cout<< "I finished" << number << "in" << number << "seconds" << std::endl;
+    std::cout<< "Hi, my name is " << name << std::endl;
+    std::cout<< "My favorite thing to do is " << activity << "." << std::endl;
+    std::cout<< "I finished my " << activity << " in " << number << " seconds. " << std::endl;
     std::cout<< "What is your favorite thing to do" << symbol << std::endl;
-    std::cout<< "Say your answer in" << decimal << "minutes!" << std::endl;
+    std::cout<< "Say your answer in " << decimal << " minutes!" << std::endl;
 
 
   return 0;
